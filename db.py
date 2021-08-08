6b4ec3239d30397e0cc0c37254e948bb1fa9b542
ReimKuos <81021221+ReimKuos@users.noreply.github.com>
2021-08-09 00:42:45 +0300

    fixing2

--- db.py
+++ db.py
@@ -2,5 +2,5 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from app import app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///user"
 db = SQLAlchemy(app)
