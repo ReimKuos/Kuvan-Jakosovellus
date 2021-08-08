b32c4595e2bec5a4f714cb3ad4b49c8b6bdb021c
ReimKuos <81021221+ReimKuos@users.noreply.github.com>
2021-08-09 00:52:09 +0300

    fixing2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from os import getenv
 
 app = Flask(__name__)
-app.secret_key = "91dc917d6aced05982d51c9828e6db20"
+app.secret_key = getenv("SECRET_KEY")
 
 import routes
 
--- db.py
+++ db.py
@@ -2,5 +2,5 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from app import app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///user"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
